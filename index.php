49f18715b0c31412922b2af42b7e85bdfbc7548c
devtest16 <nba_ballin@hotmail.com>
2016-06-27 13:28:50 -0400

    Rename index.html/php to index.php

--- index.php
+++ index.php
@@ -276,6 +276,115 @@ Microsoft HTML5 Certified(Proficient), Microsoft JavaScript Certified(Intermedia
 		<section id="hire">
     <h1>Contact </h1>
     
+    	<?php
+ 
+if(isset($_POST['email'])) {
+ 
+    $email_to = "mohsinaden@hotmail.com";
+ 
+    $email_subject = "Contact Form Submission";
+
+    function died($error) {
+ 
+        // error code
+ 
+        echo "<h1>Whoops!</h1><h2>There appears to be something wrong with your submission.</h2>";
+ 
+        echo "<strong><p>The following items are not specified correctly.</p></strong><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "<p>Return to the form and try again.</p><br />";
+		echo "<p><a href='index.php'>return to the homepage</a></p>";
+        die();
+		
+ 
+    }
+  
+ 
+    $name = $_POST['name']; // required
+ 
+    $email_from = $_POST['email']; // required
+ 
+    $comments = $_POST['comments']; // required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= '<li><p>The completed e-mail address appears to be incorrect<p></li>';
+ 
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$name)) {
+ 
+    $error_message .= '<li><p>Name appears to be entered in wrong format</p></li>';
+ 
+  }
+ 
+  if(strlen($comments) < 2) {
+ 
+    $error_message .= '<li><p>Message appears to be incorrect</p></li>';
+ 
+  }
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details are given below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+
+ 
+    $email_message .= "Name: ".clean_string($name)."\n";
+ 
+    $email_message .= "Email Adress: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+ 
+      
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+
+<h1> Thank you! </h1> <h2> Have a great day!</h2>
+
+  <?php
+ 
+}
+ 
+?>
+	
+   
+
+    
 <form action="email.php" method="post" name="contactform" enctype="text/plain">
 	      <div class="field name-box">
 		        <input maxlength="50" type="text" id="name" placeholder="Who Are You?"/>
