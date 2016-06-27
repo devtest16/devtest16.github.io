e7b730f2ef1bc5bda22a78ca67ffd881ccad689b
devtest16 <nba_ballin@hotmail.com>
2016-06-27 17:11:45 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,437 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Muhsin's portfolio site and developer hub">
+    <meta name="author" content="Muhsin Aden">
+    <title>Muhsin's Website</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/creative.css" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>s
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">HOME</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Hobbies , Goals, and Ambitions</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#portfolio">Projects</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+   
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1> W E L C O M E  </h1>
+               
+				<hr>
+                <h2> Hey there! My name's Muhsin and I'm a Front-end Web and Java developer </h2>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">ABOUT ME</h2>
+					<hr>
+                    <img class="profile-pic" src="img/profile.jpg"
+					<hr class="light">
+                    <p class="text-faded"> 
+					</br>
+					</br>
+
+Muhsin is a value driven IT Professional with a deep interest in technology and innovation. He has demonstrated direct sales experience and easily builds rapport with client prospects.  Muhsin has 
+completed a highly competitive NPower training program that provides him with the skills necessary to support today’s leading technologies.  He takes pride in his Creativity, Teamwork, 
+and maintains a strong passion for continuous learning!
+</br></br>
+My social style is expressive. I'm someone who loves to learn, an innovative thinker, and I enjoy working/pursuing success with others . I have a strong interest in Technology, Business, and Innovation (I aspire to one day
+ establish myself in all three fields).  I've gone on many trips all around the globe, and an important lesson that I've learned is  "Wherever you go, and whatever you do, life is 
+what YOU strive for and make it ". With that being said, I give maximum effort when dealing with any type of task that comes my way in life, ultimately striving for success! </br>
+</br>
+</p>                                       
+    <h3 class="bio-header">Summary of Qualifications:</h3>
+<p class="text-faded">
+										 </br>
+										 </br>
+•	2+ years’ experience programming while implementing standard practices (Organizing/Troubleshooting code, Nesting, & Commenting)
+</br>
+•	Easily able to communicate and collaborate with customers, coworkers, and supervisors
+</br>
+•	Skilled in assessing program/application performance and optimizing/improving it through design and best coding practices
+</br>
+•	5+ years customer service experience at sales & marketing environments, corporate security sites(as a security guard), and public/national/international events
+</br>
+•	Adept at investigating, examining, and solving technical programming problems 
+</br>
+</br>
+</p>
+                                          <h3 class="bio-header">Core Competencies:</h3>
+<p class="text-faded">
+										  </br>
+										  </br>
+•	Front End Development   |
+•   Java Programming     |
+•	Testing, debugging, reporting, and documenting   |
+•	Software Lifecycle Development   |
+•	Pair Programming   |
+</br>
+</br>
+</p>
+										 <h3 class="bio-header">Technical Competencies:</h3> 
+<p class="text-faded">
+</br>
+</br>
+Microsoft HTML5 Certified(Proficient), Microsoft JavaScript Certified(Intermediate), Microsoft CSS Certified(Intermediate), Oracle Java SE8 Trained(Intermediate), SQL Certfied(Intermediate),
+ C#(Beginner), Python(In Development), Mapr Big Data Analytics Certified: Hadoop Essentials (Intermediate), Mapr Big Data Analytics Certified: Developing Hadoop Applications (Beginner)
+
+ </p>
+                  
+                </div>
+            </div>
+        </div>
+    </section>
+
+	
+	
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading-2"> Hobbies, Goals, and Ambititons </h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3-col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-soccer-ball-o wow bounceIn text-primary"></i>
+                        <h3 class='hobbies'>Hobbies</h3>
+                        <p class="text-muted">I'm an all around athlete, I enjoy playing basketball, football, and baseball everynow and then. However, Soccer is my main sport! Playing sports
+					     has definitely impacted my work ethic, in the sense that it has made me more of a team player/leader/motivator and someone that generally wants success for not only himself, but all his teammates </p>
+                    </div>
+                </div>
+                <div class="col-lg-3-col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".3s"></i>
+                        <h3 class='goals'>Goals</h3>
+                        <p class="text-muted">To continuously upgrade and learn new technologies to further equip and enhance my IT skills, so I can pursue my ambitions and establish myself in the tech world </p>
+                    </div>
+                </div>
+                <div class="col-lg-3-col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-fire wow bounceIn text-primary" data-wow-delay=".5s"></i>
+                        <h3 class='ambitions'>Ambitions</h3>
+						</br></br>
+                        <p class="text-muted">I'm aiming to become a top tier full-stack developer and am really interested in getting into the Big Data field of IT
+						</br>
+						</br> Starting up an innovative tech business</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+       
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/1.gif" class="img-responsive" alt=""width="550px"height="500px">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Coming soon!
+                                </div>
+                                <div class="project-name">
+                                    Java Games, Programs, and Tools
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/2.gif" class="img-responsive" alt=""width="550px"height="500px">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Coming soon!
+                                </div>
+                                <div class="project-name">
+                                    Android Face Detection App
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/portfolio/5.gif" class="img-responsive" alt=""width="550px">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Coming Soon!
+                                </div>
+                                <div class="project-name">
+                                    Web Development Projects
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+    </section>
+
+    <aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action" >
+              
+				<h3></h3>
+               
+            </div>
+        </div>
+    </aside>
+
+	
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h3> Amazing things happen when people connect. </h3>
+						</br>
+						</br><h2 class="section-heading-2">Wanna Connect?<h2>
+                    <hr class="primary">
+                    <p>I'm currently on the market and looking for an exciting career in IT . Aiming to connect and collaborate with great minds to achieve great things! </p>
+                </div>
+				
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-phone fa-3x wow bounceIn"></i>
+                    <p>647-687-6135</p>
+                </div>
+				 <div class="col-lg-4 text-center">
+                    <i class="fa fa-linkedin fa-3x wow bounceIn"></i>
+                    <p><a href="https://ca.linkedin.com/in/theaspiringdeveloper">Muhsin Aden</a></p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".3s"></i>
+                    <p><a href="mailto:mohsinaden@hotmail.com">mohsinaden@hotmail.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+		<section id="hire">
+    <h1>Contact </h1>
+    
+	<?php
+ 
+if(isset($_POST['email'])) {
+ 
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "test@example.com";
+ 
+    $email_subject = "Contact Form Submission";
+ 
+     
+ 
+     
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "<h1>Whoops!</h1><h2>There appears to be something wrong with your submission.</h2>";
+ 
+        echo "<strong><p>The following items are not specified correctly.</p></strong><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "<p>Return to the form and try again.</p><br />";
+		echo "<p><a href='index.php'>return to the homepage</a></p>";
+        die();
+		
+ 
+    }
+  
+ 
+    $name = $_POST['name']; // required
+ 
+    $email_from = $_POST['email']; // required
+ 
+    $comments = $_POST['comments']; // required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= '<li><p>The completed e-mail address appears to be incorrect<p></li>';
+ 
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$name)) {
+ 
+    $error_message .= '<li><p>Name appears to be entered in wrong format</p></li>';
+ 
+  }
+ 
+  if(strlen($comments) < 2) {
+ 
+    $error_message .= '<li><p>Message appears to be incorrect</p></li>';
+ 
+  }
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details are given below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+
+ 
+    $email_message .= "Name: ".clean_string($name)."\n";
+ 
+    $email_message .= "Email Adress: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+ 
+      
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+
+<!-- include your own success html here -->
+
+<
+h1 > Thank you! < /h1> <h2> I'll be in touch with you as soon as possible.</h2>
+
+  <?php
+ 
+}
+ 
+?>
+	
+    <form action="email.php" method="post" name="contactform" enctype="text/plain">
+	      <div class="field name-box">
+		        <input maxlength="50" type="text" id="name" placeholder="Who Are You?"/>
+        		<label for="name">Name</label>
+		        <span class="ss-icon">check</span>
+	      </div>
+
+	      <div class="field email-box">
+		        <input maxlength="80" type="text" id="email" placeholder="your@email.com"/>
+		        <label for="email">Email</label>
+		        <span class="ss-icon">check</span>
+	      </div>
+	      <div class="field msg-box">
+		        <textarea cols="25" maxlength="1000 "id="msg" rows="4" placeholder="Your message goes here..."/></textarea>
+		        <label for="msg">Msg</label>
+		        <span class="ss-icon">check</span>
+	      </div>
+
+	      <input class="button" type="submit" value="Send" />
+  </form>
+
+  </section>
+	
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+    <h1 class="tape2">
+        <span>© Devsport 2016 </span>
+    </h1>
+	
+</body>
+
+</html>
---
+++ js/creative.js
@@ -0,0 +1,109 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+})(jQuery); // End of use strict
+var defaultAvatar = 'http://i.imgur.com/BDuNOi3.png';
+var guestuserId = Math.floor(Math.random() * (150 - 10 + 1)) + 10;
+var guestusername = '';
+
+
+
+TweenMax.staggerFromTo('.sp',.87, 
+ { drawSVG:"0% 10%" },
+ { drawSVG:"99.5% 106%", stroke:'#ff8000', opacity:1,
+  cycle:{
+    strokeWidth:function() { return R(1,5) },
+    delay:function() { return R(0,15) },
+    repeatDelay:function() { return R(2,15) }
+  },
+  ease:Power2.easeOut, repeat:-1
+}, 0);
+
+function R(min, max) { return min + Math.random() * (max - min) };
+
+// Input Lock
+$('textarea').blur(function () {
+    $('#hire textarea').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('textarea + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('textarea + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#hire .field:first-child input').blur(function () {
+    $('#hire .field:first-child input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:first-child input + label + span').css({'opacity': 0});
+        }
+    });
+});
+
+$('#hire .field:nth-child(2) input').blur(function () {
+    $('#hire .field:nth-child(2) input').each(function () {
+        $this = $(this);
+        if ( this.value != '' ) {
+          $this.addClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
+        }
+        else {
+          $this.removeClass('focused');
+          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
+        }
+    });
+});
